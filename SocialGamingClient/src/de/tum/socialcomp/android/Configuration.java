d302497e5136c06969527c86e9c9e3f96e50c461
michael.kappes <michael.kappes@tum.de>
2014-05-12 23:08:08 +0200

    client hoffentlich mal dabei ...

---
+++ SocialGamingClient/src/de/tum/socialcomp/android/Configuration.java
@@ -0,0 +1,29 @@
+package de.tum.socialcomp.android;
+
+/**
+ * Simple class that holds all configuration specific information for the game.
+ * 
+ * @author Niklas Klügel
+ *
+ */
+
+public class Configuration {
+	// TODO:  URL of the Game Server
+	public static final String ServerURL = "http://kappes.no-ip.info:9000";
+	
+	// TODO: This is the minimum distance the user should have moved to trigger a location update 
+	public static final float MinimumDistanceForLocationUpdates = 5f; 
+	
+	public static final double DefaultLongitude = 11.0;
+	public static final double DefaultLatitude = 48.0;
+	
+	/**
+	 * TODO: 
+	 * Google Cloud Messaging Sender ID:
+	 * Substitute you own sender ID here. This is the project number you got
+	 * from the API Console, as described in "Getting Started."
+	 */
+	
+	public static final String GoogleCloudMessagingSenderID = "219389446841";
+
+}
