919eaee2b40c6caa9f85a874838e46ff270ebad5
michael.kappes <michael.kappes@tum.de>
2014-05-28 15:20:14 +0200

    monster fragment geändert

--- SocialGamingClient/src/de/tum/socialcomp/android/ui/MyMonsterFragment.java
+++ SocialGamingClient/src/de/tum/socialcomp/android/ui/MyMonsterFragment.java
@@ -44,8 +44,13 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MyMonsterFragment extends Fragment {
 
@@ -118,16 +123,98 @@ public class MyMonsterFragment extends Fragment {
 						
 						
 						
-						String gameID = request.getResult().toString();
+						//String gameID = request.getResult().toString();
+						String Monster= "Fehler beim umwandeln";
+						try {
+							Monster = request.get();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
 						
 						Toast.makeText(
 								MyMonsterFragment.this.getActivity(),
-								gameID,
+								Monster,
 								Toast.LENGTH_LONG).show();
 						
 						
+						
+						
+						
 					}});
+		//array list with values to display
+		//TODO replace with monsters from database
+		HttpGetter request = new HttpGetter();
+		request.execute(new String[] { "Monsters", facebookID,
+				"getmyMonsters" });
+
+		try {
+			String requestResult = request.get();
+
+			// if we just received an empty json, ignore
+			if (requestResult.isEmpty()
+					|| !requestResult.equals("{ }")) {
+
+				JSONObject json = new JSONObject(requestResult);
+				JSONArray jsonMonsters = json
+						.getJSONArray("Monster");
+				
+				//array with the entrys for the listview
+				List<String> valueList = new ArrayList<String>();
+				
+				for (int i = 0; i < jsonMonsters.length(); i++) {
+					JSONObject jsonMonster = jsonMonsters
+							.getJSONObject(i);
+
+					String Name = jsonMonster
+							.getString("Name");
+					/*String Level = jsonMonster
+							.getString("level");
+					*/
+					valueList.add(Name/*+Level*/);
+				}
+			
+				//instantiate the list
+				ListAdapter adapter = new ArrayAdapter<String>(MyMonsterFragment.this.getActivity(),
+						android.R.layout.test_list_item, valueList);
+				final ListView lv = (ListView)rootView.findViewById(R.id.Monster_List);
+
+				lv.setAdapter(adapter);
+
+				//add the onclick listener
+				lv.setOnItemClickListener(new OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+							long arg3) {
+						//TODO create Monster fragment and open it here with the given arguments
+						
+						Toast.makeText(
+								MyMonsterFragment.this.getActivity(),
+								"clicked on"+ lv.getAdapter().getItem(arg2).toString(),
+								Toast.LENGTH_LONG).show();
+					}
+				
 					
+					
+					
+					
+				}); 
+			
+			}
+				
+			} catch (Exception e) { // various Exceptions can be
+				// thrown in the process, for
+				// brevity we do a 'catch all'
+				Log.e("Map", e.getMessage());
+			}
+
+	
+		
+		
+		
+		
+			
+		
 			
 		
 	return rootView;
